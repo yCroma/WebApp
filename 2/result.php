f734967aff885a495fbdd8d4cc9b1d190cf00fa7
Yuki Kuroma <kuroyuki0728@gmail.com>
2020-10-15 13:14:46 +0000

    FIX: POSTで受け取るよう修正

--- 2/result.php
+++ 2/result.php
@@ -18,9 +18,9 @@ function hyouji($arg){
 $user = (int)$_POST['janken']; //ユーザが入力した手を受け取る
 $com  = rand(0, 2);       //コンピュータの手を乱数で決める
 
-$win  = $_GET['win'] ?? 0;     //勝った回数
-$lose = $_GET['lose']?? 0;    //負けた回数
-$aiko = $_GET['aiko']?? 0;    //あいこだった回数
+$win  = $_POST['win'] ?? 0;     //勝った回数
+$lose = $_POST['lose'] ?? 0;    //負けた回数
+$aiko = $_POST['aiko'] ?? 0;    //あいこだった回数
 
 //勝ち負けを判定する
 if ($user == $com){
