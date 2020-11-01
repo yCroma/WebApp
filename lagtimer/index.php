61b66d1947e9f66bf1e1c2c972a026c3523d0e83
Yuki Kuroma <kuroyuki0728@gmail.com>
2020-11-01 13:52:19 +0000

    UPDATE: LPをゲームスタートページに更新した

--- lagtimer/index.php
+++ lagtimer/index.php
@@ -10,13 +10,6 @@
     </header>
     <main>
         <form action="./stop.php" method="POST">
-            <?php
-                $timezone = new DateTimeZone('Asia/Tokyo');
-                $start = new DateTime("now", $timezone);
-                // print_r($start);
-                // echo $start->format('H時i分s秒');
-            ?>
-            <input type="hidden" name="start" value="<?php print_r($start); ?>">
             <input type="submit" value="タイマースタート！！">
         </form>
     </main>
