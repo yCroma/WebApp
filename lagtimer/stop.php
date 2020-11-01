84cf74f4dc75e5ade1944d316abf41ffc6c98f08
Yuki Kuroma <kuroyuki0728@gmail.com>
2020-11-01 12:03:37 +0000

    ADD: timerの処理を追加

--- lagtimer/stop.php
+++ lagtimer/stop.php
@@ -10,8 +10,18 @@
     </header>
     <main>
         <form action="./result.php" method="POST">
+        <?php
+            $timezone = new DateTimeZone('Asia/Tokyo');
+            $end = new DateTime('now', $timezone);
+        ?>
+        <input type="hidden" name="end" value='<?php print_r($end); ?>'>
         <input type="submit" value="stop!!">
         </form>
     </main>
+    <?php
+        $start = $_POST['start'] ?? null;
+        echo($start);
+        //echo($start->format('H時i分s秒'));
+    ?>
 </body>
 </html>
