4b3acf32d0f15a03b5fa7c292024d62bb8f6b687
Yuki Kuroma <kuroyuki0728@gmail.com>
2020-11-01 10:27:17 +0000

    ADD: stop.phpのマークアップを追加

---
+++ lagtimer/stop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Stop</title>
+</head>
+<body>
+    <header>
+        <h1>Stopボタンを押してね</h1>
+    </header>
+    <main>
+        <form action="./result.php" method="POST">
+        <input type="submit" value="stop!!">
+        </form>
+    </main>
+</body>
+</html>
